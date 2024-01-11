02c9d8d6ae350748f09716eef4471029b5e2ac60
Renewdxin <cissyrenxin@icloud.com>
2024-01-11 14:45:03 +0800

    job

---
+++ internal/adapters/app/job/jobcase.go
@@ -0,0 +1,51 @@
+package job
+
+import (
+	"github.com/Renewdxin/selfMade/internal/adapters/framework/logger"
+	"github.com/Renewdxin/selfMade/internal/ports/core/job"
+	"github.com/Renewdxin/selfMade/internal/ports/framework/database"
+)
+
+type JobcaseAdapter struct {
+	core job.JobsCorePorts
+	dao  database.JobDaoPorts
+}
+
+func NewAdapter(core job.JobsCorePorts, dao database.JobDaoPorts) JobcaseAdapter {
+	return JobcaseAdapter{
+		core: core,
+		dao:  dao,
+	}
+}
+
+func (adapter JobcaseAdapter) SaveJob(job job.Job) bool {
+	if !adapter.dao.SaveJob(job) {
+		logger.Logger.Logf(logger.InfoLevel, "job: %v save error", job.ID)
+		return false
+	}
+	return true
+}
+
+func (adapter JobcaseAdapter) DeleteJob(job job.Job) bool {
+	if !adapter.dao.SaveJob(job) {
+		logger.Logger.Logf(logger.InfoLevel, "job: %v delete error", job.ID)
+		return false
+	}
+	return true
+}
+
+func (adapter JobcaseAdapter) FindJobByID(id string) job.Job {
+	return adapter.dao.FindJobByID(id)
+}
+
+func (adapter JobcaseAdapter) UpdateJob(job job.Job) bool {
+	if !adapter.dao.UpdateJob(job) {
+		logger.Logger.Logf(logger.InfoLevel, "job: %v update error", job.ID)
+		return false
+	}
+	return true
+}
+
+func (adapter JobcaseAdapter) ApplyJob() {
+
+}
--- internal/ports/app/job/jobCase.go
+++ internal/ports/app/job/jobCase.go
@@ -1,4 +1,11 @@
 package job
 
+import "github.com/Renewdxin/selfMade/internal/ports/core/job"
+
 type jobCasePorts interface {
+	SaveJob(job job.Job) bool
+	DeleteJob(job job.Job) bool
+	FindJobByID(id string) job.Job
+	UpdateJob(job job.Job) bool
+	ApplyJob()
 }
