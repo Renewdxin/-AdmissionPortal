30b87d279b581e303af19ae1c294a52b4d318f4c
Renewdxin <cissyrenxin@icloud.com>
2024-01-19 22:11:03 +0800

    admin

--- internal/adapters/app/user/adminApp.go
+++ internal/adapters/app/user/adminApp.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"github.com/Renewdxin/selfMade/internal/adapters/framework/logger"
 	jobApp "github.com/Renewdxin/selfMade/internal/ports/app/job"
 	"github.com/Renewdxin/selfMade/internal/ports/core/job"
 	"github.com/Renewdxin/selfMade/internal/ports/core/user"
@@ -10,10 +11,15 @@ import (
 type AdminAppAdapter struct {
 	job    jobApp.JobsCasePorts
 	jobDao database.JobDaoPorts
+	user   database.UserDaoPorts
 }
 
-func NewAdapter() AdminAppAdapter {
-	return AdminAppAdapter{}
+func NewAdapter(job jobApp.JobsCasePorts, jobDao database.JobDaoPorts, user database.UserDaoPorts) AdminAppAdapter {
+	return AdminAppAdapter{
+		job:    job,
+		jobDao: jobDao,
+		user:   user,
+	}
 }
 
 func (adapter AdminAppAdapter) ShowJobsDetails(id string) job.Job {
@@ -29,5 +35,8 @@ func (adapter AdminAppAdapter) ShowJobsApply() []user.User {
 }
 
 func (adapter AdminAppAdapter) ApproveJobs(u user.User) {
-
+	if !adapter.user.ChangeUserStatus(u.ID, 1-u.State) {
+		logger.Logger.Logf(logger.ErrorLevel, "failed to change user's status, userID: %v", u.ID)
+	}
+	logger.Logger.Logf(logger.InfoLevel, "change user's status, userID: %v", u.ID)
 }
--- internal/ports/framework/database/adminDB.go
+++ internal/ports/framework/database/adminDB.go
@@ -2,7 +2,7 @@ package database
 
 import "github.com/Renewdxin/selfMade/internal/ports/core/auth"
 
-type adminDBPorts interface {
+type AdminDBPorts interface {
 	SaveAdminAccount(account auth.Account) bool
 	FindAccountByID(id string) bool
 	UpdateAccount(account auth.Account) bool
