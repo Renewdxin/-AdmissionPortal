cb4b3b598daed05d9792b4dc8f971bf108385531
Renewdxin <cissyrenxin@icloud.com>
2024-01-20 20:09:38 +0800

    admin

--- internal/ports/framework/web/adminHandler.go
+++ internal/ports/framework/web/adminHandler.go
@@ -3,7 +3,7 @@ package web
 import "github.com/gin-gonic/gin"
 
 type AdminHandlerPort interface {
-	HomePage()
+	HomePage(c *gin.Context, title string)
 	ShowJobApply(c *gin.Context)
 	ShowAllJobs(c *gin.Context)
 	ShowJobDetails(c *gin.Context)
