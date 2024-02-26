bb072c4fd8843682a47f091e14e72002f6585db1
Renewdxin <cissyrenxin@icloud.com>
2024-02-26 21:50:50 +0800

    update

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -44,7 +44,7 @@ func init() {
 	}
 
 	jwtAPI := middleware.NewJWTAdapters()
-	jwtHandler := web.NewJWTHandlerAdapter(jwtAPI)
+	jwtHandler = web.NewJWTHandlerAdapter(jwtAPI)
 
 	userCore := userApp.NewUsrCoreAdapter()
 	userDao, err := database.NewUsrDaoAdapter(os.Getenv("DRIVER_NAME"), os.Getenv("DRIVER_SOURCE_NAME"), userCore)
@@ -59,7 +59,7 @@ func init() {
 	}
 
 	userAPI := user.NewUsrApplicationAdapter(userCore, userDao, mailSender, verification, redisClient, validator)
-	userHandler := web.NewUserHandlerAdapter(userAPI)
+	userHandler = web.NewUserHandlerAdapter(userAPI)
 
 	authAPI := auth.NewAuthCaseAdapter(authCore, authDao, verification, redisClient, validator, mailSender)
 	authHandler = web.NewAuthHandlerAdapter(authAPI, jwtAPI)
@@ -69,82 +69,12 @@ func init() {
 	jobCore := job.NewJobsCoreAdapter()
 	jobDao := database.NewJobsDaoAdapter(os.Getenv("DRIVER_NAME"), os.Getenv("DRIVER_SOURCE_NAME"), jobCore)
 	jobAPI := jobApp.NewJobCaseAdapter(jobCore, jobDao, userDao)
-	jobHandler := web.NewJobHandlerAdapter(jobAPI)
+	jobHandler = web.NewJobHandlerAdapter(jobAPI)
 
 	adminCore := userApp.NewAdminCoreAdapter()
 	_ = database.NewAdminDaoAdapter(os.Getenv("DRIVER_NAME"), os.Getenv("DRIVER_SOURCE_NAME"), adminCore)
 	adminAPI := user.NewAdminAppAdapter(jobAPI, jobDao, userDao)
-	adminHandler := web.NewAdminHandlerAdapter(adminAPI, jobAPI, userAPI, messageSender)
-
-	r := gin.New()
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:5173"} // 允许的前端域
-	//config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "application/json"}
-	r.Use(cors.New(config))
-	//home page
-	r.POST("/home", homeHandler.HomePage)
-
-	// auth setting
-	apiAccount := r.Group("/auth")
-	apiAccount.Use()
-	{
-		apiAccount.POST("/login", authHandler.Login)
-		//apiAccount.POST("/logout")
-		apiAccount.POST("/signup", authHandler.Register)
-		apiAccount.POST("/code/send", authHandler.CodeSend)
-		apiAccount.POST("/code/verify", authHandler.CodeVerify)
-		apiAccount.POST("/password/change/code", authHandler.ChangePasswordByCode)
-		apiAccount.POST("/password/change/pwd", authHandler.ChangePasswordByPwd)
-	}
-
-	// personal info
-	apiProfile := r.Group("/profile")
-	apiProfile.Use(jwtHandler.JWTHandler())
-	{
-		// GetUserInfo 通过id得到用户信息，返回项为姓名、性别、出生日期、邮箱、手机号
-		apiProfile.GET("/Info/:id", userHandler.GetUserInfo)
-		// DeleteUser 用户在删除前需要进行手机验证码验证才能删除
-		apiProfile.DELETE("/delete/:id", userHandler.DeleteUser)
-		// 更新用户信息，仅限手机号、邮箱
-		apiProfile.PUT("/update/:id", userHandler.UpdateUserInfo)
-		// 查询是否被录取
-		apiProfile.GET("/status/:id", userHandler.GetUserStatus)
-	}
-
-	apiJob := r.Group("/recruitment")
-	apiJob.Use()
-	{
-		//查看岗位总览
-		apiJob.GET("/jobs", jobHandler.GetJobs)
-		//查看岗位详细信息
-		apiJob.GET("/job/:jobID", jobHandler.GetJobInfo)
-		//申请投递
-		apiJob.POST("/job/:jobID/apply/:userID", jobHandler.ApplyJob)
-	}
-
-	apiAdmin := r.Group("/admin")
-	apiAdmin.Use() // 使用JWT中间件进行管理员身份验证
-	{
-		// 管理员仪表板或主页
-		apiAdmin.GET("/dashboard", adminHandler.HomePage)
-
-		// 查看所有职位发布（管理员）
-		apiAdmin.GET("/jobs", adminHandler.ShowAllJobs)
-
-		// 查看职位详情（管理员）
-		apiAdmin.GET("/job/:jobID", adminHandler.ShowJobDetails)
-
-		// 查看职位申请（管理员）
-		apiAdmin.GET("/applications/:jobID", adminHandler.ShowJobApply)
-
-		// 审批或拒绝职位申请（管理员）
-		apiAdmin.PUT("/application/:appID", adminHandler.ApproveJobs)
-	}
-
-	err = r.Run(":8080")
-	if err != nil {
-		logger.Logger.Logf(logger.FatalLevel, "falied to start : %v", err)
-	}
+	adminHandler = web.NewAdminHandlerAdapter(adminAPI, jobAPI, userAPI, messageSender)
 }
 
 func LoadENV() {
